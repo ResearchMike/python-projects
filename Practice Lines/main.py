b5a2ca949c99480d0e112b6383a3f06d7eaa5c65
resarchMike <mikegrice28@gmail.com>
2023-10-16 22:46:39 -0600

    add comment

--- Practice Lines/main.py
+++ Practice Lines/main.py
@@ -1,3 +1,5 @@
+
+
 # Multiplying the numbers in the list by two in 5 lines
 numbers = [1, 3, 5, 7, 9]
 doubled_numbers = []
@@ -9,5 +11,6 @@ print(doubled_numbers)
 # Multiplying the numbers in the list by two in 3 lines
 numbers = [1, 3, 5, 7, 9]
 doubled_numbers = [num*2 for num in numbers]
-# doubled_numbers = [num*2 for num in numbers if num > 3]
+#adding more conditions here
+# doubled_numbers = [num*2 for num in numbers if num > 3] 
 print(doubled_numbers)
